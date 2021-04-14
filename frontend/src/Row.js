3bdae01a96fb15f8fb4a19080b3e7163d21765cd
fernandoaestrella <fernandoaestrella@gmail.com>
2021-04-13 22:03:32 -0400

    change teacher response status manually

---
+++ frontend/src/Row.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+class Row extends React.Component{
+constructor(props){
+    super(props);
+    console.log(this)
+    this.state = {
+    status: props.status,
+    name: props.name,
+    email: props.email,
+    id: props.id
+    };
+}
+
+click(){
+    console.log("This is where the shell command used to send emails will be executed");
+}
+
+changeState = (id, event) => {
+    if(this.state.status == "table-success"){
+    this.setState({status: "table-danger"})
+    fetch('http://localhost:1337/professor/' + id, {
+        method: 'PATCH',
+        body: JSON.stringify({hasResponded: "false"})
+    });
+    }else{
+    this.setState({status: "table-success"})
+    fetch('http://localhost:1337/professor/' + id, {
+        method: 'PATCH',
+        body: JSON.stringify({hasResponded: "true"})
+    });
+    }
+}
+
+
+render(){
+    return(
+    <tr class={this.state.status}>
+        <th scope="row">{this.state.name}</th>
+        <td>{this.state.email}</td>
+        <td>
+        {this.state.status == "table-success" ? "Has Responded" :
+        this.state.status != "table-danger" ? "": "Has Not Responded Yet"}
+        </td>
+        <td>
+        <button onClick={this.click}>Send Reminder to Respond</button>
+        {/* <button onClick={this.changeState.bind(this, this.state.id)}>Toggle Response Status</button> */}
+        <button onClick={this.changeState.bind(this, this.state.id)}>Toggle Response Status</button>
+        </td>
+    </tr>
+    )
+}
+}
+
+  export default Row;
--- frontend/src/TeacherReminder.js
+++ frontend/src/TeacherReminder.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import './Admin.css';
 import Navigation from './Navigation.js';
+import Row from './Row.js';
 
 import { useState, useEffect } from 'react';
 
@@ -17,61 +18,8 @@ const TeacherReminder = () => {
     ).then(response=>response.json()
     ).then(data=>{ 
       setProfessors(data)
-      console.log(data)
     });
   }, [])
-
-  class Row extends React.Component{
-    constructor(props){
-      super(props);
-      this.changeState = this.changeState.bind(this);
-      this.state = {
-        status: props.status,
-        name: props.name,
-        email: props.email,
-      };
-    }
-    click(){
-      console.log("This is where the shell command used to send emails will be executed");
-    }
-
-    changeState(){
-      if(this.state.status == "table-success"){
-        this.setState({status: "table-danger"});
-      }else{
-        this.setState({status: "table-success"});
-      }
-    }
-
-    render(){
-      return(
-        <tr class={this.state.status}>
-          <th scope="row">{this.state.name}</th>
-          <td>{this.state.email}</td>
-          <td>
-            {this.state.status == "table-success" ? "Has Responded" :
-            this.state.status != "table-danger" ? "": "Has Not Responded Yet"}
-          </td>
-          <td>
-            <button onClick={this.click}>Send</button>
-            <button onClick={this.changeState}>Toggle Response Status</button>
-          </td>
-        </tr>
-      )
-    }
-  }
-
-  // function Row(props) {
-  //   return(
-  //     <tr class={props.state}>
-  //       <th scope="row">{props.name}</th>
-  //       <td>{props.last}</td>
-  //       <td>{props.email}</td>
-  //       <td>{props.state == "table-success" ? "view" :
-  //         props.state != "table-danger" ? "": "send reminder"}</td>
-  //     </tr>
-  //   );
-  // }
     
   return (
     <div className="app">
@@ -89,7 +37,7 @@ const TeacherReminder = () => {
               </tr>
             </thead>
             <tbody>
-            {professors.map(professor => <Row name={professor.name} email={professor.email} status={professor.hasResponded == "true" ? "table-success" : "table-danger"}></Row>)}
+            {professors.map(professor => <Row name={professor.name} email={professor.email} status={professor.hasResponded == "true" ? "table-success" : "table-danger"} id={professor.id}></Row>)}
             </tbody>
           </table>
         </div>
