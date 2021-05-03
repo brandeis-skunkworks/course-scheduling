75a70b8af8e0ba3ccca0c11e78fdba5d8db89640
fernandoaestrella <fernandoaestrella@gmail.com>
2021-05-02 21:26:02 -0400

    attempted to send reminder email

--- backend/api/controllers/reminder.js
+++ backend/api/controllers/reminder.js
@@ -17,61 +17,31 @@ module.exports = {
   },
 
   exits: {
-    // success: {
-    //   responseType: 'view',
-    //   viewTemplatePath: 'pages/welcome'
-    // },
-    // notFound: {
-    //   description: 'No user with the specified ID was found in the database.',
-    //   responseType: 'notFound'
-    // }
   },
 
   fn: async function ({professorId}) {
 
-    sails.log('Running custom action... (`professor reminder`) with professor ID: ' + professorId);
-    
     // Look up the user whose ID was specified in the request.
     // Note that we don't have to validate that `userId` is a number;
     // the machine runner does this for us and returns `badRequest`
     // if validation fails.
+
     var professor = await Professor.findOne({ id: professorId });
 
-    // If no user was found, respond "notFound" (like calling `res.notFound()`)
-    if (!professor) { throw 'notFound'; }
+    // await sails.helpers.sendTemplateEmail.with({
+    //   template: 'email-professor-reminder',
+    //   templateData: {
+    //     user: professor
+    //   },
+    //   to: professor.email
+    // });
 
-    // Display a personalized welcome view.
-    return;
-    //  {
-    //   name: user.name
-    // };
+
+  return;
+    // const { exec } = require("child_process");
+    // exec("echo \"When would you like to teach?\""); //, (error, stdout, stderr));
+    // The following is what was recommended to me to send an email through the command line. We just need a way to call it from this code
+    // "echo "When would you like to teach?" | mail -s "Pick your course time" -S "from=donotreply@cs.brandeis.edu" pollack@brandeis.edu"
   }
 
 };
-
-
-
-// friendlyName: 'Reminder',
-
-
-// description: 'Reminder something.',
-
-
-// inputs: {
-
-// },
-
-
-// exits: {
-
-// },
-
-
-// fn: async function (inputs) {
-
-//   sails.log('Running custom controller action (professor reminder)');
-
-//   // All done.
-//   return;
-
-// }
