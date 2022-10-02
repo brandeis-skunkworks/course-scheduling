e44eda0926eb186753fdf51e415b105b0a73ca76
Archer Heffern <archerheffern@archers-air.dyn.brandeis.edu>
2022-10-02 04:41:26 -0400

    Deleted config-store; put relevant code in App.js

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,5 +1,3 @@
-// import React, {Component} from 'react';
-// import React, { useState, useEffect } from 'react';
 import React from 'react';
 import './App.css';
 import Main from '../../Main.js';
